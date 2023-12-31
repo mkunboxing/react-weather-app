3b9b81bd0fc991f836bfa3f7a0c72bf23286f868
Mukul Kumar <mkunboxing@gmail.com>
2023-12-31 22:33:34 +0530

    bug fixed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
-import { Stack, Button, Box ,Autocomplete, TextField } from '@mui/material';
+import { Stack, Button, Box, Autocomplete, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import { getWeather } from '../services/api';
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+import { getWeather, getWeatherByCoords } from '../services/api';
 
 
-export default function Search({updateWeather }) {
+export default function Search({ updateWeather }) {
 
   const [city, setCity] = useState('');
 
@@ -22,24 +23,52 @@ export default function Search({updateWeather }) {
     }
   };
 
+  const getLocationWeather = async () => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        try {
+          const { latitude, longitude } = position.coords;
+          const response = await getWeatherByCoords(latitude, longitude);
+          updateWeather(response);
+        } catch (error) {
+          console.log(error.message);
+        }
+      });
+    } else {
+      console.log('Geolocation is not supported.');
+    }
+  };
+
   return (
     <Box sx={{ marginBottom: 2 }}>
-      <TextField onChange={(e) => handleChange(e)}
+      <TextField
+        onChange={(e) => handleChange(e)}
         id="search-input"
         label="Search City"
-        
         variant="outlined"
         size="small"
         fullWidth
         InputProps={{
           endAdornment: (
-            <Button onClick={getWeatherInfo} variant="contained" color="primary" size="small">
-              <SearchIcon />
-            </Button>
+            <React.Fragment>
+              <Button onClick={getWeatherInfo} variant="contained" color="primary" size="small">
+                <SearchIcon />
+              </Button>
+              <Button
+                onClick={getLocationWeather}
+                sx={{ marginLeft: 1 }}
+                variant="contained"
+                color="primary"
+                size="small"
+              >
+                <MyLocationIcon />
+              </Button>
+            </React.Fragment>
           ),
         }}
       />
     </Box>
+
   );
 }
 
