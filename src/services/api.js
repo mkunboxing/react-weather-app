7f6b4e47b016c86685c6ac9a785a1110e90a6fec
Mukul Kumar <108393137+mkunboxing@users.noreply.github.com>
2024-01-02 23:11:03 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 
-const API_KEY = '59a4684f9a7b2e6e2bb5d87da613d7af'; // maruga mai chori kiya toh
+const API_KEY = ''; // khud ka use kro 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
 export const getWeather = async (city) => {
